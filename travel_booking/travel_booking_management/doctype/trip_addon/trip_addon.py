ed141ff8c6c8cd64771fdc23d4e5b3621dedd9c0
diniemasjuki <diniemasjuki@gmail.com>
2026-08-28 14:06:54 +0800

    doctype edit

--- travel_booking/travel_booking_management/doctype/trip_addon/trip_addon.py
+++ travel_booking/travel_booking_management/doctype/trip_addon/trip_addon.py
@@ -17,15 +17,11 @@ class TripAddon(Document):
 		addon_organizer: DF.Link | None
 		addon_title: DF.Data | None
 		addon_type: DF.Literal["Activity", "Upgrade", "Transfer", "Other"]
-		base_price: DF.Currency
 		cover_image: DF.AttachImage | None
-		currency: DF.Link | None
 		description: DF.TextEditor | None
 		disable: DF.Check
 		naming_series: DF.Literal["TADD.YY.MM.##"]
-		scope: DF.Literal["Per Booking", "Per Pax"]
-		trip_link: DF.Link | None
-		trip_name: DF.Data | None
+		youtube_video_url: DF.Data | None
 	# end: auto-generated types
 
 	_DOCTYPE_NAME = "TripAddon"
